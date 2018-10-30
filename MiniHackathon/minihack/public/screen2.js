116dc217dab177452f2ba374478b39b4e70394f1
CAJonathan <dns140198@gmail.com>
2018-10-30 09:52:46 +0700

    aaa

--- MiniHackathon/minihack/public/screen2.js
+++ MiniHackathon/minihack/public/screen2.js
@@ -11,6 +11,17 @@ $.ajax({
             $("#player2").text(result.playerName2);
             $("#player3").text(result.playerName3);
             $("#player4").text(result.playerName4);
+
+            var gameRounds = result.rounds;
+            rounds = gameRounds.length;
+            for(let i = 0 ; i < gameRounds.length ; i ++){
+                if(i > 0) {
+                    AddRound(i);
+                }
+                for(let j = 0 ; j < 4 ; j ++){
+                    $("#" + i + j).val(gameRounds[i][j]);
+                }
+            }
         }
     },
     error: function(){
@@ -18,24 +29,28 @@ $.ajax({
     }
 })
 
-$("#addRound").on("click", function() {
-    rounds ++;
+function AddRound(round){
     var newRound =  `<tr>
-                        <th scope="col">Round ${rounds + 1}</th>
+                        <th scope="col">Round ${round + 1}</th>
                         <th scope="col" id="player1">
-                            <input id="${rounds}0" data-row="${rounds}" data-column="0" type="number" class="form-control" value=0>
+                            <input id="${round}0" data-row="${round}" data-column="0" type="number" class="form-control" value=0>
                         </th>
                         <th scope="col" id="player2">
-                            <input id="${rounds}1" data-row="${rounds}" data-column="1" type="number" class="form-control" value=0>
+                            <input id="${round}1" data-row="${round}" data-column="1" type="number" class="form-control" value=0>
                         </th>
                         <th scope="col" id="player3">
-                            <input id="${rounds}2" data-row="${rounds}" data-column="2" type="number" class="form-control" value=0>
+                            <input id="${round}2" data-row="${round}" data-column="2" type="number" class="form-control" value=0>
                         </th>
                         <th scope="col" id="player4">
-                            <input id="${rounds}3" data-row="${rounds}" data-column="3" type="number" class="form-control" value=0>
+                            <input id="${round}3" data-row="${round}" data-column="3" type="number" class="form-control" value=0>
                         </th>
                     </tr>`
     $("#rounds").append(newRound);
+}
+
+$("#addRound").on("click", function() {
+    AddRound(rounds);
+    rounds ++;
 
     $.ajax({
         url: "/addRound/",
@@ -49,22 +64,30 @@ $("#addRound").on("click", function() {
     })
 })
 
+function CalculateSum(){
+
+}
+
 $(document).on("input", ".form-control", function() {
+    var row = $(this).data("row");
+    var column = $(this).data("column")
+    var value = $(this).val()
     $.ajax({
         url: "/update",
         type: "POST",
-        data: {id: gameId, row: $(this).data("row"), column: $(this).data("column"), val: $(this).val()},
+        data: {id: gameId, row: row, column: column, val: value},
         success: function(res){
             if(res.success){
                 var sum = 0;
                 for(var i = 0 ; i < rounds + 1 ; i ++){
-                    sum += $("#" + $(this).data("row") + $(this).data("column")).val();
+                    sum += Number($("#" + i + column).val());
                 }
-                $("#player" + $(this).data("column") + "Score").text(sum);
+                $("#player" + column + "Score").text(sum);
             }
         },
         error: function(error){
             console.log(error);
         }
     })
+    console.log("Request sent")
 })
